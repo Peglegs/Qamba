4d08540b3c936434d6b9f18e9313a9bcc36471a4
coweaver <cooperweaver@mac.com>
2015-01-09 12:34:34 -0500

    fixed login bug

--- app2.py
+++ app2.py
@@ -3,7 +3,11 @@ from pymongo import Connection
 import utils
 import urllib
 import requests
-
+import os
+import json
+import time
+import sys
+import base64
 
 
 app=Flask(__name__)
@@ -18,6 +22,8 @@ OAUTH_TOKEN_URL = "https://accounts.spotify.com/api/token"
 
 UserToken = None
 
+class SpotifyOauthError(Exception):
+    pass
 
 
 @app.route("/", methods = ["GET", "POST"])
@@ -39,13 +45,18 @@ def get_authorize_url():
 
     return "%s?%s" % (OAUTH_AUTHORIZE_URL, urlparams)
 
+
 def get_token(AuthCode):
-    payload = {'client_id': CLIENT_ID,
-            'response_type': 'code',
-            'redirect_uri': REDIRECT_URI}
+    payload = {'redirect_uri': REDIRECT_URI,
+                'code': AuthCode,
+                'grant_type': 'authorization_code'}
     payload['scope'] = scope
     payload['state'] = "34fFs29kd09"
-    response = requests.post(OAUTH_TOKEN_URL,data = payload,verify=True)
+    auth_header = base64.b64encode(CLIENT_ID + ':' + CLIENT_SECRET)
+    headers = {'Authorization': 'Basic %s' % auth_header}
+
+    
+    response = requests.post(OAUTH_TOKEN_URL,data = payload, headers=headers, verify=True)
     if response.status_code is not 200:
         raise SpotifyOauthError(response.reason)
     return response.json()
@@ -56,11 +67,20 @@ def welcome():
     url = request.url
     url = url.split("code=",1)
     code = url[1]
-    get_token(code)
-    return render_template("welcome.html")
-
+    url = code.split("&",1)
+    code = url[0]
+    print "\n"
+    print  code
+    print '\n\n\n'
+    UserToken = get_token(code)
+    return render_template("welcome.html", UserToken = UserToken)
+  
 
 if __name__=="__main__":
-    app.secret_key="GetBetterGeButter"
+    app.secret_key="GetBetterGetButter"
     app.debug=True
     app.run();
+    
+
+
+
