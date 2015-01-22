1bd8bb2dac5adcf8651e3cada4ed45406718c963
ericbm2 <ericbmorgenstern@gmail.com>
2015-01-22 12:30:52 -0500

    adding about and contact and app.py change

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ app=Flask(__name__)
 
 @app.route("/")
 def home():
-    loggedin = False
     if "name" in session:
         return redirect("/profile")
     return redirect("/login")
@@ -15,7 +14,7 @@ def home():
 @app.route("/login", methods=["GET","POST"])
 def login():
     if "name" in session:
-        return redirect("/")
+        return redirect("/profile")
     if request.method=="GET":
         return render_template("login.html")
     else:
@@ -41,11 +40,14 @@ def login():
 def about():
     return render_template("about.html")
 
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
 
 @app.route("/newUser", methods=["GET", "POST"])
 def newUser():
     if "name" in session:
-        return redirect("/")
+        return redirect("/profile")
     if request.method=="GET":
         return render_template("newUser.html")
     else:
@@ -56,7 +58,7 @@ def newUser():
         error=None
         if create is True:
             session['name'] = uname
-            return redirect("/")
+            return redirect("/profile")
         else:
             error = "Sorry, the username you have selected already exists or you didn't enter a password."
             return render_template("newUser.html", error=error)
@@ -110,8 +112,11 @@ def upload():
                 return render_template("upload_failure.html")
 
 
-
-
+@app.route("/genres")
+def Genres():
+    if "name" not in session:
+        return redirect("/")
+    return render_template("genres.html")
 
 @app.route("/ArtistPage")
 def ArtistPage():
